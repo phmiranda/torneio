68dac2c5fb0529cc3c98c3f4fdb0eddea517f13a
phmiranda <phmiranda@stefanini.com>
2019-08-18 23:27:50 -0300

    [add] - inclusão do maven no projeto para facilitar a instalação de dependências nativas do Java

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    // comentários
-    public static void main(String[] args) {
-        // comentários
-        System.out.println("Olá babuíno de merda e cusão!");
-    }
-}
---
+++ src/main/java/br/com/uri/App.java
@@ -0,0 +1,13 @@
+package br.com.uri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

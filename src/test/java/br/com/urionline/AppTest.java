32ed18d928ee00852b8ea461ef7e3ae21d401317
phmiranda <pehhagah.1607@gmail.com>
2020-07-08 14:05:31 -0300

    [add] - criação dos pacotes referentes aos níveis dos desafios propostos.

---
+++ src/test/java/br/com/urionline/AppTest.java
@@ -0,0 +1,25 @@
+/**
+ * User: phmiranda
+ * Project: torneio
+ * Description: this class Q1001 execute...!
+ * Date: 07/07/2020
+ */
+
+package br.com.urionline;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue( true );
+    }
+}

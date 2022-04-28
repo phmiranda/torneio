5c7ad374e0625c9c55f62c65ec84c642c67e93d5
phmiranda <pehhagah.1607@gmail.com>
2022-04-28 08:40:52 -0300

    DEVELOPER
    Inclusão do exercício https://www.beecrowd.com.br/judge/pt/problems/view/1002

---
+++ src/main/java/br/com/phmiranda/torneio/iniciante/P1002.java
@@ -0,0 +1,18 @@
+/*
+ * Author: phmiranda
+ * Project: torneio
+ * Task Number: 1002
+ * Description: https://www.beecrowd.com.br/judge/pt/problems/view/1002
+ * Date: 28/04/2022
+ */
+
+package br.com.phmiranda.torneio.iniciante;
+
+public class P1002 {
+    public static void main(String[] args) {
+        Double area;
+        Double raio;
+        Double constanteDePi = 3.14159;
+        Double resultado;
+    }
+}
